372bfff6ff7e10708152ae0433c0cb2649a58819
“Finney <finney.solomon.1@gmail.com>
2025-10-16 21:34:51 +0530

    Index.js file

--- api/index.js
+++ api/index.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
   if (!isConnected) {
     await connectDB();
     isConnected = true;
+    console.log("isConnected")
   }
   return app(req, res);
 }
